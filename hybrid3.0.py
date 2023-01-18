c2a50356eb096d254203a3e7d3f894db6d2db6ff
divyanshbasera <divyanshbasera36@gmail.com>
2023-01-18 16:28:22 +0530

    REMOVED KEYS

--- hybrid3.0.py
+++ hybrid3.0.py
@@ -10,10 +10,6 @@ from scipy.ndimage import interpolation as inter
 
 
 # S3 BUCKET CREDS
-ACCESS_KEY = "AKIAQ34Y5OZLCCYSLY47"
-SECRET_KEY = "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"
-BUCKET_NAME ="hdfctests"
-OUTPUT_BUCKET = "hdfcoutput"
 
 # FOR LOCAL FOLDERS
 INPUT_PATH="temp_pdf"
